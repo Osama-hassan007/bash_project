d4e14ef602a3a2ebb7e09f3427b221c02017f1af
osama <mosama653@gmail.com>
2022-12-23 01:01:44 +0200

    a5er 7aga tmam+

--- DBMS.sh
+++ DBMS.sh
@@ -161,7 +161,8 @@ do
                   for ((i = 1; i <= $cn; i++)); do
                     colname=$(awk -F"|" -v"i=$i" '{if(NR==1){print $i}}' $insertb)
                     coltype=$(awk -F"|" -v"i=$i" '{if(NR==1){print $i}}' $insertb)
-                    
+                 
+				    
                     while true; do
                       read -p "Enter $colname " value
                       
@@ -174,6 +175,7 @@ do
                           echo -n $value >>$insertb
                           echo "Value 2 Added!"
                           break
+
                         fi
                       
                       
@@ -185,6 +187,51 @@ do
                 fi
 	;;
 	"select from table")
+	echo "available tables : "
+	ls -F 
+	echo "enter table name :"
+	read st 
+	typeset -i cn=$(awk -F"|" '{if(NR==1){print NF}}' $st)
+	if [[ -f $st  ]]; then
+		select selectable in "select all table" "select specific column" ;
+		do
+			case $selectable in 
+			"select all table" )
+			cat $st 
+			;;
+			"select specific column" )
+			# read -p "Enter Number of column: " number
+			# for (( i = 1; i <= number; i++ )); 
+			# do
+			# 	read -p "Enter column $number name: " colname;
+			# 	awk -F "|" '{if(NR=2){
+			# 		for(i=1 ;i <= NF; i++){
+			# 					if($i=="'$colname'"){
+			# 						var=i
+			# 				    break;
+			# 				}
+			# 			}
+			# 		} {print $var}
+			# 	}' $st
+			# done
+			for (( i=1 ; i <= $cn ; i++ )) ; do
+                        read -p "Enter the column name : " colname
+						if [[ $colname == $i  ]];then
+                        cut -d "|" -f $i $st
+						else
+						echo "$colname not exist"
+						fi
+
+            done
+			;;
+			*)
+			echo "invaled option" 
+			
+			esac
+		done
+		else 
+		echo "$st not exist "
+	fi 
 	;;
 	"delete from table")
 	;;
