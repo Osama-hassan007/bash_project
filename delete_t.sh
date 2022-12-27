31bca6b390d31804b74cf9596252b9f868a66569
osama <mosama653@gmail.com>
2022-12-27 14:49:56 +0200

    final:)

--- delete_t.sh
+++ delete_t.sh
@@ -1,23 +1,23 @@
  echo "Available tables : "
                 ls -F
 
-                read -p "Enter table Name: " TableToBeUpdated
-				if [[ -f $TableToBeUpdated ]]; then
+                read -p "Enter table Name: " TableTo_delete_from
+				if [[ -f $TableTo_delete_from]]; then
 
-                cat $TableToBeUpdated
+                cat $TableTo_delete_from
                 
-					echo "Now you are going in the vi mode to edit the file and update it, follow these instructions: "
+					echo "Now you are going in the vi mode to edit the file and delet from it, follow these instructions: "
 					echo "############################################################################################"
-					echo "1- Press i to enter the insert mode and then update the table with the value you want."
+					echo "1- Press i to enter the insert mode and then delete the table with the value you want."
 					echo "2- Press ESC to exit the insert mode and type :wq to save and exit."
 					echo "############################################################################################"
 					echo "Please confirm by pressing 1 or exit by pressing 2"
 					select confirmation in "Yes" "No" ; do
 					case $confirmation in
 					Yes)
-					vi $TableToBeUpdated
+					vi $TableTo_delete_from
 					echo "Ok! Here is the updated file:"
-					cat $TableToBeUpdated
+					cat $TableTo_delete_from
 					;;
 					No)
 					exit
@@ -28,5 +28,5 @@
 					esac
 					done
 				else 
-				echo "$TableToBeUpdated not exist"
+				echo "$TableTo_delete_from not exist"
 				fi	
